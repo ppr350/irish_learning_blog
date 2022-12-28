c4e1daffb517f1f121191f21af2fd6b68a44b2fa
ppr350 <kobito610@gmail.com>
2022-12-28 12:14:08 +0000

    Add index.html script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
